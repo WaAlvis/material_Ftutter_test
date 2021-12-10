a09b140274a877a6705843b5a79307f589f6ecd7
William Alvis <w.alvis@finextechs.com>
2021-12-09 19:21:36 -0500

    solucion temporal para el appbar del main offerts home

--- lib/pages/home/ui/components/pages_tab_mobil/create_offert/my_offerts_home.dart
+++ lib/pages/home/ui/components/pages_tab_mobil/create_offert/my_offerts_home.dart
@@ -13,11 +13,92 @@ class MyOffertsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     return Container(
       color: LdColors.white,
       child: Column(
         children: <Widget>[
-
+          Container(
+            width: size.width,
+            color: LdColors.blackBackground,
+            child: Stack(
+              alignment: AlignmentDirectional.bottomStart,
+              children: <Widget>[
+                // Esto es el circulo, ideal volverlo widget
+                Positioned(
+                  right: 0,
+                  child: SizedBox(
+                    // El tamaño depende del tamaño de la pantalla
+                    width: (size.width)/4,
+                    height: (size.width)/4,
+                    child: QuarterCircle(
+                      circleAlignment: CircleAlignment.bottomRight,
+                      color: LdColors.grayLight.withOpacity(0.05),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  child: SizedBox(
+                    width: (size.width)*2/4,
+                    height: (size.width)*2/4,
+                    child: QuarterCircle(
+                      circleAlignment: CircleAlignment.bottomRight,
+                      color: LdColors.grayLight.withOpacity(0.05),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  child: SizedBox(
+                    width: (size.width)*3/4,
+                    height: (size.width)*3/4,
+                    child: QuarterCircle(
+                      circleAlignment: CircleAlignment.bottomRight,
+                      color: LdColors.grayLight.withOpacity(0.05),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 16, top: size.height * 0.18),
+                  child: Row(
+                    children: [
+                      Text('data')
+                    ],
+                  ),
+                ),DefaultTabController(
+                  length: 2,
+                  child:
+                  TabBar(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    indicatorColor: LdColors.orangePrimary,
+                    indicatorWeight: 3,
+                    labelColor: Colors.grey,
+                    unselectedLabelColor: Colors.red,
+                    tabs: <Widget>[
+                      Tab(
+                        child: Text(
+                          'Comprar',
+                          style: textTheme.textYellow.copyWith(
+                              fontWeight: FontWeight.w400,
+                              color: LdColors.orangePrimary),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          'Vender',
+                          style: textTheme.textYellow.copyWith(
+                              fontWeight: FontWeight.w400,
+                              color: LdColors.orangePrimary),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
           Expanded(
             flex: 8,
             child: Column(
--- lib/pages/home/ui/components/pages_tab_mobil/main_offerts/main_offerts_tab.dart
+++ lib/pages/home/ui/components/pages_tab_mobil/main_offerts/main_offerts_tab.dart
@@ -18,233 +18,212 @@ class MainOffertsTab extends StatelessWidget {
     final TextTheme textTheme = Theme.of(context).textTheme;
     final Size size = MediaQuery.of(context).size;
 
-    return Column(
-      children: <Widget>[
-        Container(
-          width: size.width,
-          color: LdColors.blackBackground,
-          child: Stack(
-            alignment: AlignmentDirectional.bottomStart,
-            children: <Widget>[
-              // Esto es el circulo, ideal volverlo widget
-              Positioned(
-                right: 0,
-                child: SizedBox(
-                  // El tamaño depende del tamaño de la pantalla
-                  width: (size.width)/4,
-                  height: (size.width)/4,
-                  child: QuarterCircle(
-                    circleAlignment: CircleAlignment.bottomRight,
-                    color: LdColors.grayLight.withOpacity(0.05),
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 0,
-                child: SizedBox(
-                  width: (size.width)*2/4,
-                  height: (size.width)*2/4,
-                  child: QuarterCircle(
-                    circleAlignment: CircleAlignment.bottomRight,
-                    color: LdColors.grayLight.withOpacity(0.05),
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 0,
-                child: SizedBox(
-                  width: (size.width)*3/4,
-                  height: (size.width)*3/4,
-                  child: QuarterCircle(
-                    circleAlignment: CircleAlignment.bottomRight,
-                    color: LdColors.grayLight.withOpacity(0.05),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 16, top: size.height * 0.15),
-                child: Row(
-                  children: [
-                    Text('data')
-                  ],
-                ),
-              ),DefaultTabController(
-                length: 2,
-                child:                       TabBar(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  indicatorColor: LdColors.orangePrimary,
-                  indicatorWeight: 3,
-                  labelColor: Colors.grey,
-                  unselectedLabelColor: Colors.red,
-                  tabs: <Widget>[
-                    Tab(
-                      child: Text(
-                        'Comprar',
-                        style: textTheme.textYellow.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: LdColors.orangePrimary),
-                      ),
-                    ),
-                    Tab(
-                      child: Text(
-                        'Vender',
-                        style: textTheme.textYellow.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: LdColors.orangePrimary),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        Expanded(
-          child: Container(
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: size.width,
             color: LdColors.blackBackground,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  SvgPicture.asset(
-                    LdAssets.logo,
-                    width: 150,
-                  ),
-                  IconButton(
-                    onPressed: () => viewModel.goLogin(context),
-                    icon: const Icon(
-                      Icons.account_circle,
-                      color: LdColors.white,
-                      size: 32,
+            child: Stack(
+              alignment: AlignmentDirectional.bottomStart,
+              children: <Widget>[
+                // Esto es el circulo, ideal volverlo widget
+                Positioned(
+                  right: 0,
+                  child: SizedBox(
+                    // El tamaño depende del tamaño de la pantalla
+                    width: (size.width) / 4,
+                    height: (size.width) / 4,
+                    child: QuarterCircle(
+                      circleAlignment: CircleAlignment.bottomRight,
+                      color: LdColors.grayLight.withOpacity(0.05),
                     ),
                   ),
-                ],
-              ),
+                ),
+                Positioned(
+                  right: 0,
+                  child: SizedBox(
+                    width: (size.width) * 2 / 4,
+                    height: (size.width) * 2 / 4,
+                    child: QuarterCircle(
+                      circleAlignment: CircleAlignment.bottomRight,
+                      color: LdColors.grayLight.withOpacity(0.05),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  child: SizedBox(
+                    width: (size.width) * 3 / 4,
+                    height: (size.width) * 3 / 4,
+                    child: QuarterCircle(
+                      circleAlignment: CircleAlignment.bottomRight,
+                      color: LdColors.grayLight.withOpacity(0.05),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 16, top: size.height * 0.15),
+                  child: Row(
+                    children: [Text('data')],
+                  ),
+                ),
+                Column(
+                  children: [
+
+                    TabBar(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      indicatorColor: LdColors.orangePrimary,
+                      indicatorWeight: 3,
+                      labelColor: Colors.grey,
+                      unselectedLabelColor: Colors.red,
+                      tabs: <Widget>[
+                        Tab(
+                          child: Text(
+                            'Comprar',
+                            style: textTheme.textYellow.copyWith(
+                                fontWeight: FontWeight.w400,
+                                color: LdColors.orangePrimary),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'Vender',
+                            style: textTheme.textYellow.copyWith(
+                                fontWeight: FontWeight.w400,
+                                color: LdColors.orangePrimary),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
-        ),
-        Expanded(
-          flex: 8,
-          child:
-          TabBarView(
-                children: <Widget>[
-                  RefreshIndicator(
-                    onRefresh: () async {
-                      // keyRefresh.currentState?.show(atTop: false);
-                      await Future<Duration>.delayed(
-                        const Duration(seconds: 1),
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          OptionsFilterRow(
-                            textTheme: textTheme,
-                            quantityFilter: 2,
+          Container(
+            height: size.height*0.75,
+            child: TabBarView(
+              children: <Widget>[
+                RefreshIndicator(
+                  onRefresh: () async {
+                    // keyRefresh.currentState?.show(atTop: false);
+                    await Future<Duration>.delayed(
+                      const Duration(seconds: 1),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        OptionsFilterRow(
+                          textTheme: textTheme,
+                          quantityFilter: 2,
+                        ),
+                        const Divider(
+                          height: 8,
+                          color: LdColors.gray,
+                        ),
+                        Padding(
+                          padding:
+                          const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            'Ofertas para comprar',
+                            textAlign: TextAlign.center,
+                            style: textTheme.textBlack.copyWith(
+                                fontWeight: FontWeight.w500,
+                                color: LdColors.orangePrimary),
                           ),
-                          const Divider(
-                            height: 8,
-                            color: LdColors.gray,
+                        ),
+                        Expanded(
+                          child: ListView.separated(
+                            separatorBuilder:
+                                (BuildContext context, int index) {
+                              return const SizedBox(
+                                height: 8,
+                              );
+                            },
+                            // controller: _scrollController,
+                            itemCount: itemsBuyers.length,
+                            itemBuilder:
+                                (BuildContext context, int index) {
+                              return CardBuyAndSell(
+                                index: index,
+                                items: itemsBuyers,
+                                textTheme: textTheme,
+                                viewModel: viewModel, //Pase bien el VM
+                              );
+                            },
                           ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Text(
-                              'Ofertas para comprar',
-                              textAlign: TextAlign.center,
-                              style: textTheme.textBlack.copyWith(
-                                  fontWeight: FontWeight.w500,
-                                  color: LdColors.orangePrimary),
-                            ),
-                          ),
-                          Expanded(
-                            child: ListView.separated(
-                              separatorBuilder:
-                                  (BuildContext context, int index) {
-                                return const SizedBox(
-                                  height: 8,
-                                );
-                              },
-                              // controller: _scrollController,
-                              itemCount: itemsBuyers.length,
-                              itemBuilder:
-                                  (BuildContext context, int index) {
-                                return CardBuyAndSell(
-                                  index: index,
-                                  items: itemsBuyers,
-                                  textTheme: textTheme,
-                                  viewModel: viewModel, //Pase bien el VM
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                  RefreshIndicator(
-                    onRefresh: () async {
-                      // keyRefresh.currentState?.show(atTop: false);
-                      await Future<Duration>.delayed(
-                        const Duration(seconds: 1),
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          OptionsFilterRow(
-                              textTheme: textTheme, quantityFilter: 3),
-                          const Divider(
-                            height: 8,
-                            color: LdColors.gray,
+                ),
+                RefreshIndicator(
+                  onRefresh: () async {
+                    // keyRefresh.currentState?.show(atTop: false);
+                    await Future<Duration>.delayed(
+                      const Duration(seconds: 1),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        OptionsFilterRow(
+                            textTheme: textTheme, quantityFilter: 3),
+                        const Divider(
+                          height: 8,
+                          color: LdColors.gray,
+                        ),
+                        Padding(
+                          padding:
+                          const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            'Ofertas para vender',
+                            textAlign: TextAlign.center,
+                            style: textTheme.textBlack.copyWith(
+                                fontWeight: FontWeight.w500,
+                                color: LdColors.orangePrimary),
                           ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Text(
-                              'Ofertas para vender',
-                              textAlign: TextAlign.center,
-                              style: textTheme.textBlack.copyWith(
-                                  fontWeight: FontWeight.w500,
-                                  color: LdColors.orangePrimary),
-                            ),
+                        ),
+                        Expanded(
+                          child: ListView.separated(
+                            separatorBuilder:
+                                (BuildContext context, int index) {
+                              return const SizedBox(
+                                height: 8,
+                              );
+                            },
+                            // controller: _scrollController,
+                            itemCount: itemsSellers.length,
+                            itemBuilder:
+                                (BuildContext context, int index) {
+                              return CardBuyAndSell(
+                                index: index,
+                                items: itemsSellers,
+                                textTheme: textTheme,
+                                viewModel: viewModel,
+                              );
+                            },
                           ),
-                          Expanded(
-                            child: ListView.separated(
-                              separatorBuilder:
-                                  (BuildContext context, int index) {
-                                return const SizedBox(
-                                  height: 8,
-                                );
-                              },
-                              // controller: _scrollController,
-                              itemCount: itemsSellers.length,
-                              itemBuilder:
-                                  (BuildContext context, int index) {
-                                return CardBuyAndSell(
-                                  index: index,
-                                  items: itemsSellers,
-                                  textTheme: textTheme,
-                                  viewModel: viewModel,
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-        )
-      ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
