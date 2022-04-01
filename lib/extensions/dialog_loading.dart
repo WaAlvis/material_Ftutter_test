07b292b8e4162902b03e3f8278c4793965b2acc2
jocgomezFinex <bordonez@bitsports.co>
2022-04-01 17:49:44 -0500

    Limpieza de imports

--- lib/extensions/dialog_loading.dart
+++ lib/extensions/dialog_loading.dart
@@ -22,7 +22,9 @@ extension ShowDialogLoading on BuildContext {
               backgroundColor: LdColors.white,
               child: Padding(
                 padding: EdgeInsets.symmetric(
-                    vertical: size.width * 0.05, horizontal: size.width * 0.05),
+                  vertical: size.width * 0.05,
+                  horizontal: size.width * 0.05,
+                ),
                 child: SizedBox(
                   width: size.width * 0.1,
                   height: size.width * 0.1,
